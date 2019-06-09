72e0593e233b4db9bd339783199f2edfd3f656c6
connor bond <redconman007@gmail.com>
2019-06-09 01:01:13 -0700

    Update design.lua

--- individual/displays/induction monitor/design.lua
+++ individual/displays/induction monitor/design.lua
@@ -28,9 +28,9 @@ design.main = {
 "XXOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOXX",
 "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 }
-design.zones.main.input = {3,2,73,11}
-design.zones.main.output = {3,2,73,11}
-design.zones.main.storage = {3,15,73,24}
+design.zones.main.input = {3,2,72,11}
+design.zones.main.output = {3,2,72,11}
+design.zones.main.storage = {3,15,72,24}
 
 
 return design
